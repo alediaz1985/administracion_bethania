ee693b12b6c418167cf5dbc6419e7c3229cdbedc
alediaz1985 <alediaz1985@hotmail.com>
2024-06-10 08:25:20 -0300

    Se agregó archivo HTML docentes se midifico base se creo forms.py de docente

--- apps/administracion_docentes/urls.py
+++ apps/administracion_docentes/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.docente_list, name='docente_list'),
-    path('consultar/', views.consultar_docente, name='consultar_docente'),
-    path('registrar/', views.registrar_docente, name='registrar_docente'),
+    path('docentes/registrar/', views.registrar_docente, name='registrar_docente'),
+    path('docentes/', views.listar_docentes, name='listar_docentes'),
+    path('docentes/consultar/', views.consultar_docente, name='consultar_docente'),
+    path('docentes/editar/<str:cuil>/', views.editar_docente, name='editar_docente'),
+    path('docentes/eliminar/<str:cuil>/', views.eliminar_docente, name='eliminar_docente'),
 ]
