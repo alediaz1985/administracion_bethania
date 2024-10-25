378594f079bef2031fa4f5cd4cffb789f54b959a
Alexis Sandoval <147452729+sando96ale@users.noreply.github.com>
2024-10-24 21:23:19 -0300

    Cambio en la consulta de comprobantes

--- apps/documentos/forms.py
+++ apps/documentos/forms.py
@@ -3,6 +3,8 @@ from .models import Documento  # Asegúrate de importar el modelo Documento
 
 class ConsultaForm(forms.Form):
     consulta = forms.CharField(label='Ingrese DNI, CBU o palabra', max_length=255)
+    fecha_inicio = forms.DateField(required=False, widget=forms.TextInput(attrs={'type': 'date'}), label='Desde')
+    fecha_fin = forms.DateField(required=False, widget=forms.TextInput(attrs={'type': 'date'}), label='Hasta')
 
 class DocumentoForm(forms.ModelForm):
     class Meta:
